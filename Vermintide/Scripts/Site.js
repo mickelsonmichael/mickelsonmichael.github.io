abf8223d19519262eec8b5034c0452da7cb55fc1
Michael Mickelson <mickelsonmichael@outlook.com>
2017-11-07 17:04:54 -0700

    Added Smuggler's Run and Supply and Demand
    Smuggler's Run is just a message informing the player that there are no tomes/grimoires

--- Vermintide/Scripts/Site.js
+++ Vermintide/Scripts/Site.js
@@ -7,8 +7,20 @@ app.config(function($routeProvider, $locationProvider) {
         .when("/", {
             templateUrl: "main.htm"
         })
-        .when("/horn", {
-            templateUrl : "horn.htm"
+        .when("/HornOfMagnus", {
+            templateUrl: "pages/Act 1/HornOfMagnus.htm"
+        })
+        .when("/SmugglersRun", {
+            templateUrl: "pages/Act 1/SmugglersRun.htm"
+        })
+        .when("/SupplyAndDemand", {
+            templateUrl: "pages/Act 1/SupplyAndDemand.htm"
+        })
+        .when("/BlackPowder", {
+            templateUrl: "pages/Act 1/BlackPowder.htm"
+        })
+        .when("/WizardsTower", {
+            templateUrl: "pages/Act 1/WizardsTower.htm"
         });
 
     //$locationProvider.html5Mode(true);
