8e639a57b445b008bb18b23cf3648baf0ad7090c
Michael Mickelson <mickelsonmichael@gmail.com>
2018-10-21 10:32:22 -0400

    Added Datatables

--- Gwent/main.js
+++ Gwent/main.js
@@ -8,36 +8,62 @@ function loadCardData() {
     xhr.open("GET", "cards.json", true);
     xhr.onloadend = function() {
         parseCardData(xhr.responseText);
+
+        $("#cards-table").DataTable();
     }
 
     xhr.send();
-
 }
 
 function parseCardData(data) {
     let json = JSON.parse(data);
 
-    console.log(json);
-
     parseNeutralCards(json.cards.neutral);
 }
 
 function parseNeutralCards(cards) {
-    console.log(cards);
-
-    let tbody = document.getElementById("cards-table");
+    let tbody = document.getElementById("cards-tbody");
 
     for (let i = 0; i < cards.length; i++) {
         let card = cards[i];
 
         let row = document.createElement("tr");
 
-        let col_name = document.createElement("td");
-
-        col_name.innerText = card.name;
-
-        row.appendChild(col_name);
+        row.appendChild(createCardRow(card.name));
+        row.appendChild(createCardRow(card.power));
+        row.appendChild(createCardRow(card.ability));
+        row.appendChild(createCardRow(card.type));
+        row.appendChild(createCardRow(card.price));
+        row.appendChild(createCardRow(card.territory));
+        row.appendChild(createCardRow(card.location));
+        row.appendChild(createCardRow(card.aquired));
 
         tbody.appendChild(row);
     }
 }
+
+function createCardRow(data) {
+    let col = document.createElement("td");
+    col.innerText = data;
+
+    return col;
+}
+
+// "name": "Geralt of Rivia",
+//                 "power": 15,
+//                 "ability": "",
+//                 "type": "hero",
+//                 "price": 50,
+//                 "location": "Quest",
+//                 "territory": "Novigrad",
+//                 "quest": "Gwent: Old Friends",
+//                 "aquired": "From Thaler at the Seven Cats Inn"
+
+/* <th>Name</th>
+                                <th>Power</th>
+                                <th>Ability</th>
+                                <th>Type</th>
+                                <th>Price</th>
+                                <th>Location</th>
+                                <th>Aquisition</th>
+                                <th>Details</th> */
